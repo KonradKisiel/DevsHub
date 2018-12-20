665108a7501990b7a20e1ee78b61b294bbd08cf0
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2018-12-20 17:58:47 +0100

    connecting to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//make available modules outside of this file
+module.exports = {
+  //register to https://mlab.com and crete free mongoDB database
+  //mongoURI: "mongodb://<dbuser>:<dbpassword>@ds143262.mlab.com:43262/devshub"
+};
