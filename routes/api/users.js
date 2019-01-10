9e7b3ac860ca6e648e1d2506ac085dccb04387a6
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2019-01-10 17:18:34 +0100

    login logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,5 +51,32 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login user/ Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email })
+    .then(user => {
+      //Check for user
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+      //Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        //if password match
+        if (isMatch) {
+          res.json({ msg: 'Success' });
+        } else {
+          return res.status(400).json({ password: 'Password incorrect' });
+        }
+      })
+    });
+});
+
+
 //we have to export the router in order for the server.js to pick it up
 module.exports = router;
